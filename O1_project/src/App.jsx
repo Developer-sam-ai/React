d26da1c7374836475a21e022899b998258a01498
sam <sam.developer31@gmail.com>
2025-11-20 22:33:50 +0530

    react  color changer

--- O1_project/src/App.jsx
+++ O1_project/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [color, setColor] = useState("green");
 
   return (
-    <>
-      
-    </>
-  )
+    <div
+      className="w-full h-screen duration-150"
+      style={{ backgroundColor: color }}
+    >
+      <div className='fixed flex flex-wrap'></div>
+    </div>
+
+  );
 }
 
 export default App
