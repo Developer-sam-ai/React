efbddca7b4b345d951e1cb0a0187e0f4eaf1cb79
sam <sam.developer31@gmail.com>
2025-11-21 23:07:30 +0530

    minor

--- O1_project/src/App.jsx
+++ O1_project/src/App.jsx
@@ -5,13 +5,19 @@ function App() {
   const [color, setColor] = useState("green");
 
   return (
+    <>
     <div
       className="w-full h-screen duration-150"
       style={{ backgroundColor: color }}
     >
       <div className='fixed flex flex-wrap'></div>
+      <div className=''>
+        <button>hello</button>
+      </div>
     </div>
 
+    
+    </>
   );
 }
 
