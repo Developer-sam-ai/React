c8a15ef7cc98b2198855e405b4b8ede5a63ab44b
sam <sam.developer31@gmail.com>
2025-11-22 22:58:01 +0530

    start of project

---
+++ O2_password_generator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>password generator</h1>
+    <div className='bg'>
+
+    </div>
+    </>
+  )
+}
+
+export default App
