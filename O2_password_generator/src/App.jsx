1cff822083a5fd3b798e320812900099fb4ba0e3
sam <sam.developer31@gmail.com>
2025-11-24 16:57:34 +0530

    password gen

--- O2_password_generator/src/App.jsx
+++ O2_password_generator/src/App.jsx
@@ -1,9 +1,26 @@
-// import { useState } from 'react'
+import { useState,useCallback } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [length,setlength]=useState(8);
+  const [numberallowed,setnumberallowed]=useState(false);
+  const [charcterallowed,Setcharcacterallowed]=useState(false);
+  const [password,Setpassword]=useState("")
 
+  const passwordgenrator=useCallback(()=>{
+    let res=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberallowed) str+='0123456789';
+    if(charcterallowed) str+='!@#$%^&*()';
+
+    for(let i=1;i<=length;i++){
+      let cha=Math.floor(Math.random()*str.length+1)
+      res=str.charAt(cha);
+    }
+    Setpassword(res);
+  },[length,numberallowed,charcterallowed, Setpassword])
+
+  
   return (
     <>
         {/* <h1 className="flex items-center justify-center bg-red-400 p-3  w-fit ">password generator</h1> */}
@@ -11,9 +28,11 @@ function App() {
   password generator
 </h1>
 
-    <div className='bg-gray-800 w-full h-screen flex justify-center'>
-      
-      
+    <div className='bg-gray-800 w-full flex justify-center mx-auto shadow-md h-32 px-1 my-1 rounded-3xl text-red-400 w-fit'>
+      <div className=' rounded-lg overflow-hidden mb-4'>
+        <input type='text' value={password} className='outline-none w-full py-1 px-4'
+        placeholder='password'/>
+      </div>
     </div>
     </>
   )
