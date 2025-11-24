15d08980df548f09cebd754f48d8aa0ae6268683
sam <sam.developer31@gmail.com>
2025-11-24 15:39:55 +0530

    react password gen

--- O2_password_generator/src/App.jsx
+++ O2_password_generator/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>password generator</h1>
-    <div className='bg'>
+        {/* <h1 className="flex items-center justify-center bg-red-400 p-3  w-fit ">password generator</h1> */}
+        <h1 className="text-center bg-gray-700 p-4 text-xl font-bold">
+  password generator
+</h1>
 
+    <div className='bg-gray-800 w-full h-screen flex justify-center'>
+      
+      
     </div>
     </>
   )
