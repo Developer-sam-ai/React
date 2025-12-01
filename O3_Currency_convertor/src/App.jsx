8e88381b8884ca5a1769e9f989b39c2fcc1c6358
sam <sam.developer31@gmail.com>
2025-12-01 22:53:16 +0530

    final react with tailwind

--- O3_Currency_convertor/src/App.jsx
+++ O3_Currency_convertor/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h2 className='bg-red-400'>helllo</h2>
     </>
   )
 }
