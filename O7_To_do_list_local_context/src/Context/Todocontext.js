48c95629784683f9d6ad98101112466c2e08041c
sam <sam.developer31@gmail.com>
2026-01-09 00:05:21 +0530

    Initialize React Vite to-do list project
    Set up a new React project using Vite with local context for to-do list management. Includes initial configuration files, context setup, basic app structure, Tailwind CSS, ESLint, and project assets.

---
+++ O7_To_do_list_local_context/src/Context/Todocontext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+
+
+export const Todocontext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:'Todo msg',
+            completed:false
+        }
+    ],
+    addtodo:(todo)=>{},
+    updatetodo:(id,todo)=>{},
+    deletetodo:(id)=>{},
+    togglecomplete:(id)=>{}
+});
+
+export const usetodo=()=>{
+    return useContext(Todocontext)
+}
+export const todoprovider=Todocontext.Provider
