03d8a211da54e82ef9d360dfb263ed3953c94691
sam <sam.developer31@gmail.com>
2026-01-14 17:21:12 +0530

    Add Updating_Objects component with state management
    Introduced the Updating_Objects component to manage and update nested object state for a person and their artwork. Updated App.jsx to render this new component and added a corresponding CSS file.

--- O8_Small_projects_many/src/App.jsx
+++ O8_Small_projects_many/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Updating_Objects from './Components/Updating_Objects'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+  <Updating_Objects/>
     </>
   )
 }
--- O8_Small_projects_many/src/Components/Updating_Objects.jsx
+++ O8_Small_projects_many/src/Components/Updating_Objects.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+useState
 
+import './Updating_Objects'
 function Updating_Objects() {
+
+const [person,setperson]=useState({
+  name:"Radha",
+  artwork:{
+    title:'Binkamachi',
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1p340oBhFrA7EZygpSCXeL3Ve0pt5UYZ15Q&s",
+    city:"pune"
+  }
+})
+
+  function handleName(){
+    setperson({
+      ...person,
+      
+    })
+  }
+
+  function handleTitle(e){
+    setperson({
+      //  we have to open them seperately one for 
+      ...person,
+      artwork:{
+        ...person.artwork,
+        title:e.target.value
+      }
+    })
+  }
+  function handleCity(e){
+      setperson({
+        ...person,
+        artwork:{
+          ...person.artwork,
+          city:e.target.value
+        }
+      })
+  }
+  function handleImage(e){
+    setperson({
+      ...person,
+      artwork:{
+        ...person.artwork,
+        image:e.target.value
+      }
+    })
+  }
   return (
     <>
-    
+      <div> Name:
+      <input type= 'text'  value={person.name} onChange={handleName}/>
+      </div>
+      <div>Title: 
+        <input type="text" value={person.artwork.title} onChange={handleTitle} />
+      </div>
+      <div>City:
+        <input type="text"  value={person.artwork.city} onChange={handleCity}/>
+      </div>
+      <div> image:
+        <input type="text"  value={person.artwork.image} onChange={handleImage}/>
+      </div>
+      <div>{"info"}</div>
+      <div>
+        <i>we have our cutie named {person.name} from {person.artwork.city} city</i>
+        <i>being the best supportor always but she is ekdam{person.artwork.title}</i>
+        <img src={person.artwork.image} alt="" />
+      </div>
+
     </>
   )
 }
